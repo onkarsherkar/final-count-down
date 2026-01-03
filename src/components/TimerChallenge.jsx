1dab0dee66e22d51c9d9ad42bc1f9c8d0924ce7d
onkarsherkar <onkar.sherkar@gmail.com>
2026-01-03 15:59:53 +0530

    Use ref to control dom element

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimeChallenge({ title, targetTime }) {
+  const timer = useRef();
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
   function handleStart() {
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpired(true);
     }, targetTime * 1000);
     setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
   }
 
   return (
@@ -19,7 +24,7 @@ export default function TimeChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStart}>
+        <button onClick={timerStarted ? handleStop : handleStart}>
           {timerStarted ? "Stop" : "Start"} challenge
         </button>
       </p>
